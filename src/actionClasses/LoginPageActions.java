a6b022dd2b6f40764d8c440c44b994ee26d0bdee
DigvijayQAIT <digvijay.singh@qainfotech.com>
2018-06-26 02:08:11 +0530

    Test Cases For GMail Login

--- src/actionClasses/LoginPageActions.java
+++ src/actionClasses/LoginPageActions.java
@@ -8,20 +8,28 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-
 /*
  * @author Digvijay
  */
 
-
 public class LoginPageActions {
-	
-	WebDriver driver;
-	JavascriptExecutor js;
-	
+
+	private WebDriver driver;
+	private JavascriptExecutor js;
+	private WebElement emailField, emailNext, passwordField, passwordNext, emailHeading, passwordHeading;
+	private String email, password, emailHeadingText, passwordHeadingText;
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public LoginPageActions(WebDriver driver) {
 		this.driver = driver;
-		js = (JavascriptExecutor)this.driver;
+		js = (JavascriptExecutor) this.driver;
 	}
 
 	public void launchLoginPage(String string) {
@@ -30,24 +38,57 @@ public class LoginPageActions {
 	}
 
 	public void verifyLoginPageLaunched() {
+		emailHeading = (WebElement) js.executeScript("return document.getElementById(\"headingText\");");
+		emailHeadingText = emailHeading.getText();
 		WebElement logo = driver.findElement(By.xpath("//div[@id = 'logo']"));
 		Assert.assertTrue(logo.isDisplayed());
 		System.out.println("User is on Login Page");
 	}
 
-	public void sendEmailAndClickNext() {
-		js.executeScript("document.getElementById('identifierId').value='vj.vns1707';");
-//		js.executeScript("document.getElementById('identifierNext').click();");
-		WebElement emailNext = (WebElement) js.executeScript("return document.getElementById('identifierNext')");
+	public void sendEmailAndClickNext(String userEmail) {
+		setEmail(userEmail);
+		emailField = (WebElement) js.executeScript("return document.getElementById('identifierId');");
+		emailField.sendKeys(userEmail);
+		emailNext = (WebElement) js.executeScript("return document.getElementById('identifierNext')");
 		emailNext.click();
 		System.out.println("User entered email and clicked next");
 	}
 
-	public void sendPasswordAndLogin() {
+	public void verifyEmailEnteredAndNextClicked() {
+		String email = emailField.getAttribute("value");
+		Assert.assertEquals(this.email, email);
+		System.out.println("Email entered - Verified");
+
 		WebDriverWait wait = new WebDriverWait(driver, 5);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
-		js.executeScript("document.querySelector('input[type = \"password\"]').value = 'myCROWsoft@1831';");
-		js.executeScript("document.getElementById('passwordNext').click();");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text() = 'Forgot password?']")));
+		passwordHeading = (WebElement) js.executeScript("return document.getElementById(\"headingText\");");
+		passwordHeadingText = passwordHeading.getText();
+		Assert.assertNotEquals(emailHeadingText, passwordHeadingText);
+		System.out.println("Next Button Clicked");
+
 	}
 
+	public void sendPasswordAndLogin(String userPassword) {
+		setPassword(userPassword);
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+		passwordField = (WebElement) js.executeScript("return document.querySelector('input[type = \"password\"]');");
+		passwordField.sendKeys(userPassword);
+		passwordNext = (WebElement) js.executeScript("return document.getElementById('passwordNext')");
+		passwordNext.click();
+		System.out.println("User entered password and clicked next");
+	}
+
+	public void verifyPasswordEnteredAndUserLogined(String expectedURL) {
+		String password = passwordField.getAttribute("value");
+		Assert.assertEquals(this.password, password);
+		System.out.println("Password entered - Verified");
+
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@class = 'gb_1a']")));
+		String actualURL = driver.getCurrentUrl();
+		Assert.assertEquals(actualURL, expectedURL);
+		System.out.println("User Logined");
+		System.out.println();
+	}
 }
--- src/testClasses/GMailTest.java
+++ src/testClasses/GMailTest.java
@@ -9,24 +9,26 @@ import org.testng.annotations.Test;
 import actionClasses.LoginPageActions;
 
 public class GMailTest {
-	
+
 	WebDriver driver;
 	LoginPageActions loginPage;
-	
+
 	@BeforeClass
 	public void initVars() {
 		driver = new ChromeDriver();
 		loginPage = new LoginPageActions(driver);
 	}
-	
+
 	@Test
 	public void Step01_Launch_LoginPage() {
 		loginPage.launchLoginPage("https://mail.google.com");
 		loginPage.verifyLoginPageLaunched();
-		loginPage.sendEmailAndClickNext();
-		loginPage.sendPasswordAndLogin();
+		loginPage.sendEmailAndClickNext("vj.vns1707");
+		loginPage.verifyEmailEnteredAndNextClicked();
+		loginPage.sendPasswordAndLogin("myCROWsoft@1831");
+		loginPage.verifyPasswordEnteredAndUserLogined("https://mail.google.com/mail/u/0/#inbox");
 	}
-	
+
 	@AfterClass
 	public void closeBrowser() {
 		driver.close();
